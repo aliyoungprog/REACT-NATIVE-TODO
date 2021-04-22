8b4defad78a13b8866710997279735e7d54cc28c
Aliyoungprog <abdurahmanov1231@gmail.com>
2021-04-22 16:37:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+// Abdurahmanov Abduali ID: 20MD0117
 
-function App() {
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import Task from './components/Task';
+
+export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const addTask = () => {
+    setTaskItems([...taskItems, task])
+    setTask(null);
+    Keyboard.dismiss();
+  }
+
+  const deleteTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={appStyles.container}>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+
+      <View style={appStyles.tasksBody}>
+        <Text style={{fontSize: 24, fontWeight: 'bold', color:"blue"}}>Task tracker by Naz</Text>
+        <View style={appStyles.items}>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index}  onPress={() => deleteTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+
+      </ScrollView>
+
+      <KeyboardAvoidingView
+        style={appStyles.writeTaskBody}
+      >
+        <TextInput style={appStyles.input} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => addTask()}>
+          <View style={appStyles.addBody}>
+            <Text>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+    </View>
   );
 }
 
-export default App;
+const appStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFC0CB',
+  },
+  tasksBody: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  items: {
+    marginTop: 16,
+  },
+  writeTaskBody: {
+    position: 'absolute',
+    bottom: 80,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    backgroundColor: '#E6E6FA',
+    borderRadius: 40,
+    borderColor: 'blue',
+    borderWidth: 1,
+    width: 300,
+  },
+  addBody: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'blue',
+    borderWidth: 1,
+  },
+});
